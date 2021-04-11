7fc48208a73f40a332ce47fd79aed3689186d481
Kalil Holanda <kalilholanda@me.com>
2021-04-11 00:13:50 -0300

    Added QuestionViewController and options tableView

---
+++ QuizApp/QuestionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Kalil Holanda on 10/04/21.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var question: String = ""
+    private var options: [String] = []
+    
+    convenience init(question: String, options: [String]) {
+        self.init()
+        self.question = question
+        self.options = options
+    }
+    
+    override func viewDidLoad() {
+        headerLabel.text = question
+    }
+}
+
+extension QuestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+    
+
+}
